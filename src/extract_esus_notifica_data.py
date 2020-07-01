cdda14578dc737168ba778b104e7647425e33391
Marcus Carvalho <marcuswac@gmail.com>
2020-06-30 23:30:54 -0300

    fix and split by state

--- src/extract_esus_notifica_data.py
+++ src/extract_esus_notifica_data.py
@@ -2,28 +2,35 @@ from datetime import datetime
 from elasticsearch import Elasticsearch, helpers
 import csv
 import pandas as pd
+import asyncio
 
-es = Elasticsearch('https://elasticsearch-saps.saude.gov.br',
-                   http_auth=('user-public-notificacoes', 'Za4qNXdyQNSa9YaA'))
+async def write_results(res_df, uf):
+    csv_file = 'notificacoes_esusve_{}.csv.gz'.format(uf.lower())
+    res_df.to_csv(csv_file, index=False, quoting = csv.QUOTE_NONNUMERIC)
+    print('Data written to: {}'.format(csv_file))
 
-query = {
-	"sort" : [
-        { "dataAtualizacao" : {"order" : "desc"}}
-       ]
-}
+ufs = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+       "PB", "PE", "PI", "PR", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
 
-res = es.search(index = 'desc-notificacoes-esusve-*', body = query,
-                scroll = '1m', size = 10000)
-scroll_id = res['_scroll_id']
+for uf in ufs:
+#for uf in ["complete"]:
+    es = Elasticsearch('https://elasticsearch-saps.saude.gov.br',
+                    http_auth=('user-public-notificacoes', 'Za4qNXdyQNSa9YaA'))
 
-print("Got %d Hits Total" % res['hits']['total']['value'])
-res_hits = []
+    index = 'desc-notificacoes-esusve-' + uf.lower()
+    #index = 'desc-notificacoes-esusve-*'
 
-while len(res['hits']['hits']):
-    res_hits += [x['_source'] for x in res['hits']['hits']]
-    print("Hits read: %d" % len(res_hits))
+    res = es.search(index = index, scroll = '1m', size = 10000)
     scroll_id = res['_scroll_id']
-    res = es.scroll(scroll_id  = scroll_id, scroll = '1m')
 
-res_df = pd.DataFrame(res_hits)
-res_df.to_csv('notificacoes_esusve.csv.gz', index=False)
+    print('Data for {}. Total hits: {}'.format(uf, res['hits']['total']['value']))
+    res_hits = []
+
+    while len(res['hits']['hits']):
+        res_hits += [x['_source'] for x in res['hits']['hits']]
+        print("Hits read: %d" % len(res_hits))
+        scroll_id = res['_scroll_id']
+        res = es.scroll(scroll_id  = scroll_id, scroll = '1m')
+
+    res_hits = pd.DataFrame(res_hits)
+    asyncio.run(write_results(res_hits, uf))
