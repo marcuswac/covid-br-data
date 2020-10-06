8d66598bf82e666f184beed76aa4b4dc4f18c6b9
Marcus Carvalho <marcuswac@gmail.com>
2020-10-06 08:40:17 -0300

    removing old dependencies

--- src/extract_covid_data_br.py
+++ src/extract_covid_data_br.py
@@ -1,16 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import glob
-import pandas as pd
-import sys
-import time, os
 import json
-import requests
+import os
+import sys
 from datetime import datetime, timedelta
 
-def download_ms_data2(download_dir):
+import pandas as pd
+import requests
+
+def download_ms_data(download_dir):
     print("Downloading file to", download_dir)
     url = "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/"
     headers = {
@@ -35,49 +31,6 @@ def download_ms_data2(download_dir):
     df.to_csv(downloaded_file, index=False)
     return(downloaded_file)
 
-def download_ms_data(download_dir):
-    print("Downloading file to", download_dir)
-    xlsx_files = glob.glob(os.path.join(download_dir, "HIST_PAINEL_COVIDBR_*.xlsx"))
-    #xlsx_files = glob.glob(os.path.join(download_dir, "HOJE_PAINEL_COVIDBR_*.xlsx"))
-    n_before = len(xlsx_files)
-
-    # To prevent download dialog
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference('browser.download.folderList', 2) # custom location
-    profile.set_preference('browser.download.manager.showWhenStarting', True)
-    profile.set_preference('browser.download.dir', download_dir)
-    profile.set_preference('browser.helperApps.neverAsk.saveToDisk',
-                           'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-
-    driver = webdriver.Firefox(profile)
-    driver.get("https://covid.saude.gov.br")
-
-    time.sleep(5)
-    button_xpath = "/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button"
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, button_xpath)))
-    element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
-
-    element.click()
-    
-    # Wait for file to be created
-    sleep_time = 10 # seconds
-    timeout = 120 # seconds
-    attempts = 0
-    while len(xlsx_files) == n_before and attempts * sleep_time <= timeout:
-        print("Waiting +10 seconds to finish downloading...")
-        time.sleep(10)
-        xlsx_files = glob.glob(os.path.join(download_dir, "HIST_PAINEL_COVIDBR_*.xlsx"))
-        #xlsx_files = glob.glob(os.path.join(download_dir, "HOJE_PAINEL_COVIDBR_*.xlsx"))
-
-    xlsx_files.sort(key=os.path.getmtime, reverse=True)
-    downloaded_file = xlsx_files[0]
-    print("File downloaded:", downloaded_file)
-   
-    time.sleep(10)
-
-    driver.quit()
-    return(downloaded_file)
-
 def filter_country(df):
     df_country = df[df.estado.isnull() & df.codmun.isnull()]
     return(df_country)
@@ -92,10 +45,6 @@ def filter_cities(df):
 
 def export_csvs(csv_file, csv_prefix):
     df_complete = pd.read_csv(csv_file)
-    #df_complete = pd.read_excel(excel_file)
-    #csv_file = csv_prefix + "-complete.csv"
-    #df_complete.to_csv(csv_file, index=False)
-    #print("CSV complete exported:", csv_file)
 
     df = filter_country(df_complete)
     csv_file = csv_prefix + "-country.csv"
@@ -118,7 +67,7 @@ def main(args):
     csv_filename_prefix = args[1] if len(args) > 1 else "covid-br-ms"
     csv_output_prefix = os.path.join(download_dir, csv_filename_prefix)
 
-    downloaded_file = download_ms_data2(download_dir)
+    downloaded_file = download_ms_data(download_dir)
     export_csvs(downloaded_file, csv_output_prefix)
 
 if __name__ == '__main__':
