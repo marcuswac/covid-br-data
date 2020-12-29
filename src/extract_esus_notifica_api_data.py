0eaa8806c234a6fef1717cc655c52d25c9de6199
Marcus Carvalho <marcuswac@gmail.com>
2020-12-28 21:16:09 -0300

    getting all fields that appears in first 1M rows

--- src/extract_esus_notifica_api_data.py
+++ src/extract_esus_notifica_api_data.py
@@ -1,42 +1,36 @@
 from elasticsearch import Elasticsearch, helpers
-import csv
 from joblib import Parallel, delayed
-import gzip
+import pandas as pd
+
+def write_row(res_list, csv_file, i, buffer_size):
+    res_df = pd.DataFrame(res_list)
+    if i <= buffer_size:
+        res_df.to_csv(csv_file)
+    else:
+        res_df.to_csv(csv_file, mode='a', header=False)
 
 def extract_uf(uf):
     es = Elasticsearch('https://elasticsearch-saps.saude.gov.br',
                     http_auth=('user-public-notificacoes', 'Za4qNXdyQNSa9YaA'))
 
     index = 'desc-notificacoes-esusve-' + uf.lower()
-
-    fields = ['source_id',
-              #'dataSintomas', 'dataRegistro', 'dataAtualizacao',
-              'municipio', 'estado', 'municipioNotificacao', 'estadoNotificacao',
-              #'comorbidades', 'numeroCnes', 'confirmado',
-              'sexo', 'idade', 
-              #'febre', 'sinaisGravidade', 'sinaisRespiratorios',
-              'dataTeste', 'tipoTeste', 'classificacaoFinal',
-              #'sintomas', 'profissionalSaude','profissionalSeguranca', 'cbo', 
-              'dataNotificacao', 
-              #'outrosSintomas', 'estrangeiro', 'racaCor',
-              'evolucaoCaso', 'estadoTeste',
-              #'condicoes',
-              'dataEncerramento', 'dataInicioSintomas', 'resultadoTeste',
-              #'testeSorologico', 'dataTesteSorologico', 'tipoTesteSorologico', 'resultadoTesteSorologicoIgA',
-              #'resultadoTesteSorologicoIgG', 'resultadoTesteSorologicoIgM', 'resultadoTesteSorologicoTotais',
-              '_updated_at', '@timestamp',
-              #'estadoIBGE', 'estadoNotificacaoIBGE',
-              'municipioIBGE', 'municipioNotificacaoIBGE']
-    res_scan = helpers.scan(es, index = index, _source = fields, scroll = '30m', size = 10000)
+    
+    res_scan = helpers.scan(es, index = index, scroll = '30m', size = 10000)
     csv_file_name = 'esus-notifica/api/notificacoes_esusve_{}.csv.gz'.format(uf.lower())
     print("Starting to extract data for {}".format(uf))
     
-    with gzip.open(csv_file_name, "wt") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fields)
-        writer.writeheader()
-        writer.writerows((document['_source'] for document in res_scan))
-    
-    print("Finished to extract data for {}".format(uf))
+    buffer_size = 1000000
+    res_list = []
+    i = 0
+    for result in res_scan:
+        res_list.append(result['_source'])
+        i += 1
+        if i % buffer_size == 0:
+            write_row(res_list, csv_file_name, i, buffer_size)
+            print("Written {} rows for {}".format(i, uf))
+            res_list = []
+    write_row(res_list, csv_file_name, i, buffer_size)  
+    print("Finished to extract data for {}. Total rows: {}".format(uf, i))
 
 ufs = ["SP", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
        "PB", "PE", "PI", "PR", "RJ", "RN", "RS", "RO", "RR", "SC", "SE", "TO"]
